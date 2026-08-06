54690a31259f8e329ec67b726b110112fd863b97
liam1661 <liamoberg13@hotmail.com>
2026-08-06 22:26:43 +0200

    artist

---
+++ data/artists.js
@@ -0,0 +1,31 @@
+const artists = [
+  {
+    id: "gilli",
+    name: "Gilli",
+    image: "assets/images/artists/gilli.jpg",
+    genre: "Rap",
+    debut: 2014,
+    country: "Danmark",
+    monthlyListeners: "1.900.000"
+  },
+
+  {
+    id: "branco",
+    name: "Branco",
+    image: "assets/images/artists/branco.jpg",
+    genre: "Rap",
+    debut: 2011,
+    country: "Danmark",
+    monthlyListeners: "980.000"
+  },
+
+  {
+    id: "node",
+    name: "Node",
+    image: "assets/images/artists/node.jpg",
+    genre: "Rap",
+    debut: 2015,
+    country: "Danmark",
+    monthlyListeners: "700.000"
+  }
+];
