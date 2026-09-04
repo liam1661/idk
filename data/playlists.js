c460bf1be28df6b0f50d67008eef81a1464712a8
liam1661 <liamoberg13@hotmail.com>
2026-09-04 21:11:50 +0200

    fejl fix måske

--- data/playlists.js
+++ data/playlists.js
@@ -1,42 +1,73 @@
 const playlists = [
+
     {
         id: 1,
+
         name: "Mørk & Nattekørsel",
+
         description: "Mørke og melodiske tracks til natten.",
+
         creator: "MusikBasen",
+
         color: "night",
+
         songs: [1, 3, 4]
     },
+
     {
         id: 2,
+
         name: "Rå Gaderap",
+
         description: "Dansk gaderap med rå energi.",
+
         creator: "MusikBasen",
-        color: "street"
+
+        color: "street",
+
         songs: [1, 2, 3]
     },
+
     {
         id: 3,
+
         name: "Efter Midnat",
+
         description: "Musik til de sene timer.",
+
         creator: "MusikBasen",
-        color: "midnight"
+
+        color: "midnight",
+
         songs: [1, 4]
     },
+
     {
         id: 4,
+
         name: "Fest & Sommer",
+
         description: "Danske tracks til fest og sommer.",
+
         creator: "MusikBasen",
-        color: "summer"
+
+        color: "summer",
+
         songs: [1, 2, 4]
     },
+
     {
         id: 5,
+
         name: "Flex & Livsstil",
+
         description: "Rap, attitude og livsstil.",
+
         creator: "MusikBasen",
-        color: "flex"
+
+        color: "flex",
+
         songs: [1, 2]
     }
+
 ];
